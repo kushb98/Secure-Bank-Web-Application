d130e9ee3bad52d6bd0d76c5d44031ca31057802
kush0698 <kb97@njit.edu>
2023-04-29 14:33:55 -0400

    adding accounts and user table modifications

---
+++ Milestone1/sql/007_insert_table_accounts.sql
@@ -0,0 +1,8 @@
+INSERT IGNORE INTO
+    IS601_Users (
+        id,
+        email, 
+        username, 
+        password
+    )
+VALUES (-1,'system_kb97@mail.com', 'system_kb97', '$2b$12$wRt2/iPKMDy3XQ1rDL1eg.8bnl5AG3KD2jWOW48Qnsb/oAYHG/xHC');
