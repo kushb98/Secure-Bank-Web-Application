68ca39d11b99724c1dfefebb266c806a3fa0a0a1
kush0698 <kb97@njit.edu>
2023-04-29 16:03:40 -0400

    Addings NAV bar and Forms

---
+++ Milestone1/accounts/forms.py
@@ -0,0 +1,11 @@
+import datetime
+from flask_wtf import FlaskForm
+from wtforms import TextAreaField, SelectField, SubmitField, DateField, FloatField, StringField
+from wtforms.validators import DataRequired, NumberRange, InputRequired
+
+class CreateAccountForm(FlaskForm):
+    acc_type_choices = [('Checking', 'Checking Account'), ('Savings', 'Savings Account'), ('Fixed Deposit', 'Fixed Deposit Account'), ('Money Market', 'Money Market Account'), ('Certificates of Deposit', 'Certificates of Deposit Account')]
+    acc_type = SelectField("Account Type", choices=acc_type_choices, validators=[DataRequired()])
+    creation_date = DateField("Date", validators=[InputRequired()])
+    funds = FloatField("Initial Deposit funds", validators=[InputRequired(),NumberRange(min=5)])
+    submit = SubmitField("Create")
--- Milestone1/sql/007_insert_table_accounts.sql
+++ Milestone1/sql/007_insert_table_accounts.sql
@@ -6,3 +6,12 @@ INSERT IGNORE INTO
         password
     )
 VALUES (-1,'system_kb97@mail.com', 'system_kb97', '$2b$12$wRt2/iPKMDy3XQ1rDL1eg.8bnl5AG3KD2jWOW48Qnsb/oAYHG/xHC');
+
+INSERT IGNORE INTO
+    IS601_K_Accounts (
+        id,
+        account_number, 
+        account_type,
+        user_id
+    )
+VALUES (-1,'000000000000', 'World', -1);
