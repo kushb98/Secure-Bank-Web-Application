0bbfee7ee5e6203151b1742cd813d235ceb1c68c
kush0698 <kb97@njit.edu>
2023-04-29 23:24:21 -0400

    adding creat account route

--- Milestone1/sql/006_create_table_accounts.sql
+++ Milestone1/sql/006_create_table_accounts.sql
@@ -1,12 +1,14 @@
-CREATE TABLE IF NOT EXISTS IS601_K_Accounts(
+DROP TABLE IF EXISTS IS601_Accounts;
+
+CREATE TABLE IF NOT EXISTS IS601_Accounts(
     id int AUTO_INCREMENT PRIMARY KEY,
-    account_number varchar(12) unique DEFAULT (LPAD(user_id, 12, "0")),
+    account_number varchar(12) unique,
     user_id int NOT NULL,
-    balance DECIMAL NOT NULL DEFAULT 0.0,
-    account_type varchar(12) UNIQUE,
+    balance DECIMAL NOT NULL DEFAULT 0.00,
+    account_type varchar(50),
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
     -- this won't work for Bank project as this causes 1:1 account to user
     FOREIGN KEY (user_id) REFERENCES IS601_Users(id),
-    check (balance >= 0.0 AND LENGTH(account_number) = 12)
+    check (balance >= 0.0 AND LENGTH(account_number)=12)
 )
